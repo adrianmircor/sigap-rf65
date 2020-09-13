33ab14d711052bd753b602cca9ce724e9af02a20
Carlos Mirano <carlos.mirano@hotmail.com>
2020-09-13 18:02:56 -0500

    Implementacion del requerimiento RF61.

---
+++ src/Configuracion/Config.js
@@ -0,0 +1,4 @@
+// const CONFIG = "https://sigapdev2-consultarecibos-back.herokuapp.com/";
+//const CONFIG= 'http://localhost:8080/';
+const CONFIG = "https://sigap-back-dev.herokuapp.com/";
+export default CONFIG;
